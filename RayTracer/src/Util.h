8866261eaafe569bb54ca1f2cb4f914c0ec163c1
Oliver Bailey Smith <smitholiverbailey@gmail.com>
2023-01-24 16:47:14 -0500

    Add basic diffuse material

--- RayTracer/src/Util.h
+++ RayTracer/src/Util.h
@@ -6,6 +6,9 @@
 
 #include <cstdlib>
 
+#include "vendor/glm/ext/vector_float3.hpp"
+#include "vendor/glm/gtx/norm.hpp"
+
 // Usings
 
 using std::shared_ptr;
@@ -19,18 +22,35 @@ const float PI = 3.1415926535897932385f;
 
 // Utility Functions
 
-inline float degrees_to_radians(float degrees) {
+inline float degreesToRadians(float degrees) {
     return degrees * PI / 180.0f;
 }
 
-inline float random_float() {
+inline float randomFloat() {
     // Returns a random real in [0,1).
     return rand() / (RAND_MAX + 1.0);
 }
 
-inline float random_float(float min, float max) {
+inline float randomFloat(float min, float max) {
     // Returns a random real in [min,max).
-    return min + (max - min) * random_float();
+    return min + (max - min) * randomFloat();
+}
+
+inline glm::vec3 randomVec3() {
+    return glm::vec3(randomFloat(), randomFloat(), randomFloat());
+}
+
+inline glm::vec3 randomVec3(float min, float max) {
+    return glm::vec3(randomFloat(min, max), randomFloat(min, max), randomFloat(min, max));
+}
+
+inline glm::vec3 randomInUnitSphere() {
+    while (true) {
+        glm::vec3 p = randomVec3(-1.0f, 1.0f);
+        if (glm::length2(p) >= 1)
+            continue;
+        return p;
+    }
 }
 
 inline float clamp(float x, float min, float max) {
--- RayTracer/src/main.cpp
+++ RayTracer/src/main.cpp
@@ -11,12 +11,16 @@
 #include "vendor/glm/ext/vector_int3.hpp"
 #include "vendor/glm/gtx/vector_angle.hpp"
 
-glm::vec3 rayColour(const Ray &ray, const HittableObject &scene) {
+glm::vec3 rayColour(const Ray &ray, const HittableObject &scene, uint32_t maxRayBounces) {
 
 	HitRecord hitRecord;
 
+	if (maxRayBounces <= 0)
+		return glm::vec3(0.0f, 0.0f, 0.0f);
+
 	if (scene.Hit(ray, 0.0f, POSITIVE_INFINITY, hitRecord)) {
-		return 0.5f * (hitRecord.m_Normal + glm::vec3(1.0f, 1.0f, 1.0f));
+		glm::vec3 diffuseTarget = hitRecord.m_HitPoint + hitRecord.m_Normal + randomInUnitSphere();
+		return 0.5f * rayColour(Ray(hitRecord.m_HitPoint, diffuseTarget - hitRecord.m_HitPoint), scene, maxRayBounces - 1);
 	}
 
 	glm::vec3 unitDirection = glm::normalize(ray.GetDirection());
@@ -33,6 +37,7 @@ void main() {
 	const int32_t IMAGE_WIDTH = 400;
 	const int32_t IMAGE_HEIGHT = (int32_t)(IMAGE_WIDTH / IMAGE_ASPECT_RATIO);
 	const int32_t SAMPLES_PER_PIXEL = 100;
+	const int32_t MAX_RAY_BOUNCES = 50;
 
 	// Scene
 
@@ -59,11 +64,11 @@ void main() {
 			glm::ivec3 pixelColour(0, 0, 0);
 			for (int s = 0; s < SAMPLES_PER_PIXEL; s++) {
 
-				float u = ((float)x + random_float()) / (float)(IMAGE_WIDTH - 1);
-				float v = ((float)y + random_float()) / (float)(IMAGE_HEIGHT - 1);
+				float u = ((float)x + randomFloat()) / (float)(IMAGE_WIDTH - 1);
+				float v = ((float)y + randomFloat()) / (float)(IMAGE_HEIGHT - 1);
 
 				Ray ray = camera.getRay(u, v);
-				pixelColour += (glm::ivec3)(rayColour(ray, scene) * 255.999f);
+				pixelColour += (glm::ivec3)(rayColour(ray, scene, MAX_RAY_BOUNCES) * 255.999f);
 
 			}
 
